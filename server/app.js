c68f9be9770c1eae89d233ba22601d547d76b025
Wendrel_Moraes <wendrelmorae@gmail.com>
2026-01-15 15:45:22 -0300

    Initi do gemini ficou pra tras

--- server/app.js
+++ server/app.js
@@ -30,6 +30,8 @@ import looksRoutes from './routes/looksRouter.js';
 dotenv.config();
 //Mongo Init
 connectDB();
+// Inicializar Gemini
+initGemini();
 
 const app = express();
 app.set('trust proxy', 1);
